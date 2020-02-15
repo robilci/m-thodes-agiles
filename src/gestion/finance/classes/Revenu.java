f2624921d8e6871683f8c05a0984a18522ac3d8e
Beni MULUMBA <kimkayna@gmail.com>
2020-02-15 18:36:34 +0100

    Ajout des classes - à valider

---
+++ src/gestion/finance/classes/Revenu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion.finance.classes;
+
+/**
+ *
+ * @author robin
+ */
+public class Revenu extends Transaction{
+    
+    private String type;
+
+    public Revenu(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Revenu --> " + super.toString() + " - Type : " + this.type;
+    }
+    
+}
--- src/gestion/finance/projet/GestionFinanceProjet.java
+++ src/gestion/finance/projet/GestionFinanceProjet.java
@@ -15,7 +15,7 @@ public class GestionFinanceProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("gestion.finance.projet.GestionFinanceProjet.main()");
     }
     
 }
