34586fb184e33fecb5e3e9fd9c81f2202eed02f7
Beni MULUMBA <kimkayna@gmail.com>
2020-02-27 12:46:51 +0100

    modification sql

--- data.sql
+++ data.sql
@@ -1,3 +1,11 @@
+DELETE FROM categorie;
+INSERT INTO `categorie` (`ID_Categorie`, `Nom`) VALUES
+('1', 'Materiel'),
+('2', 'Nourriture'),
+('3', 'Services'),
+('4', 'Santé'),
+('5', 'Autre');
+
 DELETE FROM compte;
 INSERT INTO compte VALUES ("1", "admin", "1234");
 
@@ -7,7 +15,7 @@ INSERT INTO personne VALUES("2", "MARFIL", "Victor", "1997-07-09", "1");
 
 
 DELETE FROM transactions;
-INSERT INTO transactions VALUES("1", "Dépense", "2019-04-12", "1500", "Achat voiture", "1");
-INSERT INTO transactions VALUES("2", "Dépense", "2019-04-13", "58.55", "Restaurant Chinois (Coronavirus gratuit)", "1");
-INSERT INTO transactions VALUES("3", "Revenue", "2019-04-28", "2456.96", "Salaire", "2");
-INSERT INTO transactions VALUES("4", "Dépense", "2019-04-12", "20.30", "Chargeur iphone", "2");
+INSERT INTO transactions VALUES("1", "Dépense", "2019-04-12", "1500", "Achat voiture", "1", "1");
+INSERT INTO transactions VALUES("2", "Dépense", "2019-04-13", "58.55", "Restaurant Chinois (Coronavirus gratuit)", "1", "2");
+INSERT INTO transactions VALUES("3", "Revenue", "2019-04-28", "2456.96", "Salaire", "2", "5");
+INSERT INTO transactions VALUES("4", "Dépense", "2019-04-12", "20.30", "Chargeur iphone", "2", "1");
--- structure.sql
+++ structure.sql
@@ -35,13 +35,6 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   PRIMARY KEY (`ID_Categorie`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `categorie`
---
-
-INSERT INTO `categorie` (`ID_Categorie`, `Nom`) VALUES
-('1', 'testhamza'),
-('2', 'aussiMOI');
 
 -- --------------------------------------------------------
 
@@ -54,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `compte` (
   `ID_Compte` INT NOT NULL AUTO_INCREMENT,
   `Pseudo` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
+  CONSTRAINT compte_unique UNIQUE (Pseudo, Password),
   PRIMARY KEY (`ID_Compte`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -101,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `Montant` decimal(15,2) DEFAULT NULL,
   `Libelle` varchar(50) DEFAULT NULL,
   `ID_Personne` int(11) DEFAULT NULL,
-  'ID_Categorie' int(11) DEFAULT NULL,
+  `ID_Categorie` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID_Transaction`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 COMMIT;
